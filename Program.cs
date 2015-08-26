c1a7687fd64b5936ea54506baae26034ed4d4a1f
alsuk <alsuk@bk.ru>
2015-08-26 13:42:26 +0300

    Запрос города для определения погоды

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace parseYandexWeather
     {
         static void Main(string[] args)
         {
+            // id города, нужен для получения ссылок с прогнозом
+            string id = "";
+            Console.WriteLine("Введите город, погоду в котором хотите узнать");
+            string city = Console.ReadLine();
+            // Первую букву делаем заглавной, остальные строчные
+            city = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(city);
         }
     }
 }
