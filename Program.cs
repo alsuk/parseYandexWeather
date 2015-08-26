3c631a8a0fad3c35a6dd98688a8d39c46e287d8d
alsuk <alsuk@bk.ru>
2015-08-26 13:52:29 +0300

    Добавляем метод, получающий ID города

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
 
 namespace parseYandexWeather
 {
@@ -16,6 +13,31 @@ namespace parseYandexWeather
             string city = Console.ReadLine();
             // Первую букву делаем заглавной, остальные строчные
             city = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(city);
+            GetID(city, id);
+        }
+
+        /// <summary>
+        /// Получаем id города
+        /// </summary>
+        /// <returns>id</returns>
+        static string GetID(string city,string id)
+        {
+            XmlDocument xml = new XmlDocument();
+            // Тащим таблицу городов с айдишниками
+            xml.Load("https://pogoda.yandex.ru/static/cities.xml");
+            XmlNode root = xml.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("//city");
+            // Ищем интересующий город и достаем id
+            foreach (XmlNode node in nodes)
+            {
+                if (node.InnerText == city)
+                {
+                    id = node.Attributes[0].Value;
+                    Console.WriteLine(node.Attributes[0].Value + "\t" + node.InnerText);
+                }
+            }
+            // Возвращаем id города
+            return id;
         }
     }
 }
